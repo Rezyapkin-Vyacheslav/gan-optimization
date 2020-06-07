0bd6e3a9f14510f58f3dd01b395b15c9aa7cdfe1
Aleksei Pronkin <31882933+alexey-pronkin@users.noreply.github.com>
2020-06-07 21:43:11 +0300

    Update grad_sliding.pyi

--- optim/grad_sliding.pyi
+++ optim/grad_sliding.pyi
@@ -1,4 +1,4 @@
 from torch.optim.optimizer import _params_t, Optimizer
 
-class QuickPropSGD(Optimizer):
+class GradSliding(Optimizer):
     def __init__(self, params: _params_t, L: float, M: float, D_tilde: float) -> 
