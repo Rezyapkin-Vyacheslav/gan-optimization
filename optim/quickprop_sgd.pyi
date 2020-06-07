9b6e6b97d2470e4f9f1b9cfbbdaaf341f42941be
Aleksei Pronkin <31882933+alexey-pronkin@users.noreply.github.com>
2020-06-07 21:46:02 +0300

    Update quickprop_sgd.pyi

--- optim/quickprop_sgd.pyi
+++ optim/quickprop_sgd.pyi
@@ -1,4 +1,4 @@
 from torch.optim.optimizer import _params_t, Optimizer
 
 class QuickPropSGD(Optimizer):
-    def __init__(self, params: _params_t, lr: float, momentum: float=..., dampening: float=..., weight_decay:float=..., nesterov:bool=..., eps:float=...) -> 
+    def __init__(self, params: _params_t, lr: float, weight_decay:float=..., eps:float=...) -> 
